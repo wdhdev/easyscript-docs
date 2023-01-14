4f4b7a82971c5905fd8cb30682c45ebc3a634104
William Harrison <william@williamdavidharrison.com.au>
2023-01-14 16:29:38 +0800

    better meta

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,16 +10,23 @@ const config: DocsThemeConfig = {
   },
  head: (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Easy Script" />
-      <meta
-        property="og:image"
-        content="https://images.easyscript.dev/banner.png"
-      />
-      <meta
-        property="og:description"
-        content="A npm package which makes coding in JavaScript easy!"
-      />
+      <title>Easy Script Docs</title>
+      <meta name="description" content="The official documentation for Easy Script." />
+
+      <meta itemprop="name" content="Easy Script Docs" />
+      <meta itemprop="description" content="The official documentation for Easy Script." />
+      <meta itemprop="image" content="https://images.easyscript.dev/banner.png" />
+
+      <meta property="og:url" content="https://docs.easyscript.dev" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Easy Script Docs" />
+      <meta property="og:description" content="The official documentation for Easy Script." />
+      <meta property="og:image" content="https://images.easyscript.dev/banner.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Easy Script Docs" />
+      <meta name="twitter:description" content="The official documentation for Easy Script." />
+      <meta name="twitter:image" content="https://images.easyscript.dev/banner.png" />
     </>
   ),
   // logo: (<><img src="https://images.easyscript.dev/default.png" height="64px" /></>),
